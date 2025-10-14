49d584c5f4adb61f19210bef0126569d7b7cf26e
Dhamar ⊬ <cabreradhamar@ciencias.unam.mx>
2025-10-13 21:16:35 -0600

    add: TestAFDmin - Se incluyeron las pruebas para el archivo AFDmin

--- src/Test.hs
+++ src/Test.hs
@@ -10,6 +10,8 @@
 module Main where
 
 import Regex
+import AFD
+import AFDmin
 
 import Test.QuickCheck -- Para hacer pruebas con aleatorieidad
 import Control.Exception (try, evaluate, SomeException) -- Para manejar las excepciones esperadas en las pruebas
@@ -103,8 +105,55 @@ pruebasgetRegex = forAll genRegexString $ \(s, shouldBeValid) ->
                  Left _  -> not shouldBeValid  -- falló, debió ser inválida
 
 
+
 -- ------------------------------------------------------------------------------
--- Ejecucion de 1000 Pruebas Aleatorias
+-- =============================================================================
+--                                Pruebas AFDmin
+-- =============================================================================
+-- ------------------------------------------------------------------------------
+-- Definicion de un AFD para minimizar
+afdEjemplo = AFD
+  { estadosD = ["q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7"]
+  , alfabetoD = ['a', 'b']
+  , transicionesD =
+      [ ("q1",'a',"q2"), ("q1",'b',"q3")
+      , ("q2",'a',"q4"), ("q2",'b',"q5")
+      , ("q3",'a',"q6"), ("q3",'b',"q7")
+      , ("q4",'a',"q4"), ("q4",'b',"q5")
+      , ("q5",'a',"q6"), ("q5",'b',"q7")
+      , ("q6",'a',"q4"), ("q6",'b',"q5")
+      , ("q7",'a',"q6"), ("q7",'b',"q7")
+      ]
+  , inicialD = "q1"
+  , finalD = ["q2", "q6"]
+  }
+
+-- Prueba: eliminar estados inalcanzables
+testUnreachables :: IO ()
+testUnreachables = print $ removeUnreachable afdEjemplo
+
+-- Prueba: refinamiento de particiones
+testGroups :: IO ()
+testGroups = print $ groupEquivalents afdEjemplo
+
+-- Prueba: construcción completa del AFD mínimo
+testAFDmin :: IO ()
+testAFDmin = print $ minimizaAFD afdEjemplo
+
+
+-- ------------------------------------------------------------------------------
+-- Ejecucion de Pruebas
 -- ------------------------------------------------------------------------------
 main :: IO ()
-main = quickCheckWith stdArgs { maxSuccess = 1000 } pruebasgetRegex
+main = do
+  -- REGEX --
+  putStrLn "---- Pruebas Expresiones Regulares ----"
+  quickCheckWith stdArgs { maxSuccess = 1000 } pruebasgetRegex
+
+  -- AFDmin --
+  putStrLn "\n---- Eliminación de inalcanzables ----"
+  testUnreachables
+  putStrLn "\n---- Grupos equivalentes ----"
+  testGroups
+  putStrLn "\n---- AFD mínimo ----"
+  testAFDmin
